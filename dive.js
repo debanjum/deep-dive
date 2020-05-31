168a1b369ee93e83e9fdbf27ab4b0a11eed80951
debanjum <debanjum@gmail.com>
2020-05-30 21:30:01 -0700

    Do not try deep dive when not visiting deep link. Clean dive.js code

--- dive.js
+++ dive.js
@@ -1,8 +1,14 @@
-// Extract text to dive to from URL
 // Dive text from URL in ScrollToTextFragment Format
 // Refer https://github.com/WICG/ScrollToTextFragment#tldr
-var dive_to_text = decodeURI(window.location.hash
-                             .split(':~:text=')[1]);
+const delimiter = ":~:text=";
 
-// Find, scroll to and highlight the text on page
-window.find(dive_to_text);
+// If visiting in a deep link URL
+if (window.location.hash.includes(delimiter))
+{
+  // Extract text to dive to from URL
+  var dive_to_text = decodeURI(window.location.hash
+                               .split(delimiter)[1]);
+
+  // Find, scroll to and highlight the text on page
+  window.find(dive_to_text);
+}
